5faa12b071c9e1b1eda0dad30ffa11698c8d3273
Carlos Vargas <varlopecar@gmail.com>
2022-03-11 21:36:13 -0600

    Operations

---
+++ app.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+function operate(operation, a, b){
+    return operation(a, b);
+}
