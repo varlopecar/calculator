07462bcedb2153d14d28462929202447b72b7949
Carlos Vargas <varlopecar@gmail.com>
2022-03-12 08:21:40 -0600

    Finish

--- app.js
+++ app.js
@@ -6,6 +6,32 @@ const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
 
-function operate(operation, a, b){
-    return operation(a, b);
-}
+const operate = (operation, a, b) => operation(a, b);
+
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
