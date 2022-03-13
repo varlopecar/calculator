605a9ac3f94b80946ead4ff3380fe44bcc61cee1
Carlos Vargas <varlopecar@gmail.com>
2022-03-12 18:27:55 -0600

    Format

--- app.js
+++ app.js
@@ -8,30 +8,30 @@ const divide = (a, b) => a / b;
 
 const operate = (operation, a, b) => operation(a, b);
 
-let display = document.getElementById('display');
+let display = document.getElementById("display");
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+let buttons = Array.from(document.getElementsByClassName("button"));
 
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        break;
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = "Error";
         }
-    });
+        break;
+      case "←":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
 });
